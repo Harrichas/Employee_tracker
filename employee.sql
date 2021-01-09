6c2cf697921b0ead74ecacd6d66a90dd3a68a396
Chase Harrington <acechaseharrington@gmail.com>
2021-01-09 10:03:51 -0600

    made manager id a foreign key

--- employee.sql
+++ employee.sql
@@ -21,6 +21,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30), 
     last_name VARCHAR(30), 
     role_id INT, 
-    manager_id INT 
-    FOREIGN KEY (role_id)
+    manager_id INT,
+    FOREIGN KEY (role_id),
+    FOREIGN KEY (manager_id)
 );
