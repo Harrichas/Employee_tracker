b559635c430efb61745ce4be9f08c64156f37b25
Chase Harrington <acechaseharrington@gmail.com>
2021-01-09 19:31:12 -0600

    add more to queries and prompts

--- app.js
+++ app.js
@@ -1,36 +1,6 @@
-const mysql = require('mysql2');
-const [
-    View_All_Employees,
-    Veiw_All_Employees_by_Department,
-    View_All_Employees_by_Manager,
-    View_All_Roles,
-    View_All_Departments,
-    View_Total_Budget_by_Department,
-    Update_Employee_Manager,
-    Update_Employee_Role,
-    Add_Employee,
-    Add_Role,
-    Add_Department,
-    Remove_Role,
-    Remove_Employee,
-    Remove_Department] = require('./lib/const');
-const {promptChoices} = require('./lib/prompt');
-// const  {} = require('./lib/queries');
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'employeeDB',
-});
-
-connection.connect(async (err) => {
-    if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
-    //await ();
-    connection.end();
-});
+const constants = require('./lib/const');
+const prompts = require('./lib/prompt');
+const  {} = require('./lib/queries');
 
 // async function () {
 
@@ -39,3 +9,4 @@ connection.connect(async (err) => {
 //   answer = await promptChoices();
 
 // }
+prompts.promptChoices();
