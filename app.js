97ae9eeacf6f48dff04b6b983991e7e4c4adb68c
Chase Harrington <acechaseharrington@gmail.com>
2021-01-10 19:48:02 -0600

    can see employee by department

--- app.js
+++ app.js
@@ -2,11 +2,5 @@ const constants = require('./lib/const');
 const prompts = require('./lib/prompt');
 const  {} = require('./lib/queries');
 
-// async function () {
 
-//     let ;
-
-//   answer = await promptChoices();
-
-// }
 prompts.promptChoices();
